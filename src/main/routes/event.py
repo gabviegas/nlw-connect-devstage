5ae8da653f2de80b70b7b3151721c285809119ba
Gabriel Viégas <viegasr.edu@gmail.com>
2025-02-19 16:55:07 -0300

    feat: Implementing http types

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,9 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 # Cria um agregador de rotas relacionadas a eventos
 event_route_bp = Blueprint("event_route", __name__)
 
+from src.http_types.http_response import httpResponse
+from src.http_types.http_request import httpRequest
+
 # Cria uma rota "event" com o método POST
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
-    return jsonify({"estou": "aqui"}), 201
+    http_request = httpRequest(body=request.json) # seleciona o body do request de .html
+
+    http_response = httpResponse(body={ "estou" : "aqui"}, status_code=201)
+    
+    return jsonify(http_response.body), http_response.status_code
