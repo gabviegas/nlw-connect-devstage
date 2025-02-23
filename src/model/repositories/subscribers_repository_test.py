d72233cfa586c6408d85638c3e337a1f06b4b12c
Gabriel Viégas <viegasr.edu@gmail.com>
2025-02-23 16:03:41 -0300

    feat: Adding new methods in subscribers_repository

--- src/model/repositories/subscribers_repository_test.py
+++ src/model/repositories/subscribers_repository_test.py
@@ -4,9 +4,10 @@ from .subscribers_repository import SubscribersRepository
 @pytest.mark.skip('Inserção no DB')
 def test_insert():
     subs_info = {
-        "name": "meu nome",
+        "name": "Gabs",
         "email": "email@email.com",
         "evento_id": 2,
+        "link": "hiii"
     }
 
     subs_repo = SubscribersRepository()
@@ -20,3 +21,14 @@ def test_select_subscriber():
     subs_repo = SubscribersRepository()
     selection = subs_repo.select_subscriber(email, evento_id)
     print(selection.nome)
+
+@pytest.mark.skip('Ranking de links do DB')
+def test_ranking():
+    evento_id = 7
+    subs_repo = SubscribersRepository()
+    resp = subs_repo.get_ranking(evento_id)
+
+    print()
+    for item in resp:
+        print(f"Link:{item.link}, Total de inscritos:{item.total}")
+    
